195ba6b2ddb7d9d108c1ac0f9224fbb74a0cf87c
Ian Rudge <ianmrudge@gmail.com>
2021-12-05 14:37:23 -0700

    day 4 part 2

---
+++ 4/4.2.js
@@ -0,0 +1,88 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+const { chunk } = require('lodash');
+
+const main = async () => {
+  const input = await fs.readFile(join(__dirname, 'input.txt'), { encoding: 'utf-8' });
+  const data = input.split('\n');
+
+  let [drawNums, ...boardData] = data;
+
+  drawNums = drawNums.split(',').map((v) => parseInt(v, 10));
+
+  boardData = boardData.filter((v) => v);
+  boardData = chunk(boardData, 5);
+
+  const MARK = 'X';
+
+  let boards = boardData.map((data) => {
+    return data.map((row) => {
+      return row
+        .split(' ')
+        .filter((v) => v)
+        .map((v) => parseInt(v, 10));
+    });
+  });
+
+  const markBoard = (board, num) => {
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i];
+      for (let j = 0; j < row.length; j++) {
+        if (row[j] === num) {
+          row[j] = MARK;
+        }
+      }
+    }
+    return board;
+  };
+
+  const checkForWin = (board) => {
+    // check horizontal
+    for (let i = 0; i < board.length; i++) {
+      if (board[i].every((v) => v === MARK)) {
+        return board;
+      }
+    }
+
+    // check vertical
+    for (let i = 0; i < board.length; i++) {
+      const col = [];
+      for (let j = 0; j < board[i].length; j++) {
+        col.push(board[j][i]);
+      }
+      if (col.every((v) => v === MARK)) {
+        return board;
+      }
+    }
+  };
+
+  let winningBoards = [];
+  let draw;
+
+  while (drawNums.length !== 0 && boards.length !== 0) {
+    draw = drawNums.shift();
+    boards = boards.map((b) => markBoard(b, draw));
+    boards.forEach((b, idx) => {
+      if (checkForWin(b)) {
+        winningBoards.push(...boards.splice(idx, 1));
+      }
+    });
+  }
+
+  const lastWinningBoard = winningBoards.pop();
+  const sum = lastWinningBoard.reduce((acc, row) => {
+    return (
+      acc +
+      row.reduce((rowSum, num) => {
+        return num !== MARK ? rowSum + num : rowSum;
+      }, 0)
+    );
+  }, 0);
+
+  console.log(sum * draw);
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
